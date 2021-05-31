2d7c7ee0ef62edfd4fa69bafb3e9786577fa5bb3
Jan <jan.mail@q.com>
2021-05-31 10:00:12 +0800

    memtool

--- mylib/memtool.c
+++ mylib/memtool.c
@@ -9,15 +9,15 @@ void usage( void )
     printf( "\t\tmemtool 0xA0000000 20\n" );
 }
 
-void print( long int addr, long int cnt )
+void print( long int addr, unsigned int cnt )
 {
-    printf( "Reading %.8x count starting at addres 0x%.8x\n\n", cnt, addr );
-    printf( "0x%.8x: 0x%.8x 0x%.8x 0x%.8x 0x%.8x\r\n", addr, ( ( int * )( addr ) )[0], ( ( int * )( addr ) )[1], ( ( int * )( addr ) )[2], ( ( int * )( addr ) )[3] );
+    printf( "Reading %d count starting at addres 0x%.8lx\n\n", cnt, addr );
+    printf( "0x%.8lx: 0x%.8x 0x%.8x 0x%.8x 0x%.8x\r\n", addr, ( ( int * )( addr ) )[0], ( ( int * )( addr ) )[1], ( ( int * )( addr ) )[2], ( ( int * )( addr ) )[3] );
 }
 
 int main( int argc, char **argv )
 {
-    long int cnt;
+    unsigned int cnt;
     long int addr;
 
     if ( argc != 3 )
@@ -26,9 +26,8 @@ int main( int argc, char **argv )
         return 1;
     }
 
-    cnt = ( long int )strtol( argv[2], 0, 16 );
+    cnt = ( unsigned int )strtol( argv[2], 0, 16 );
     addr = ( long int )strtol( argv[1], 0, 16 );
-    printf( "Address: %s cnt:%d\n", argv[2], cnt );
-    print( &addr, cnt );
+    print( addr, cnt );
     return 0;
 }
